7bda3681eba28d5181ece9f2641b6d13fd972b9c
Seungjin Yang <slowmoyang@gmail.com>
2024-03-25 14:58:23 +0900

    fix to use absolute path

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-export PROJECT_PREFIX=$(dirname ${BASH_SOURCE})
+export PROJECT_PREFIX=$(realpath $(dirname ${BASH_SOURCE}))
 export PYTHONPATH=${PROJECT_PREFIX}/src:${PYTHONPATH}
 micromamba activate diffmet-py311
