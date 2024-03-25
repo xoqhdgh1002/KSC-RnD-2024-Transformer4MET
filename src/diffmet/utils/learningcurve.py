592112de9494147a819e51399c51681efebd1617
Seungjin Yang <slowmoyang@gmail.com>
2024-03-25 14:57:37 +0900

    replace tab with spaces

--- src/diffmet/utils/learningcurve.py
+++ src/diffmet/utils/learningcurve.py
@@ -46,15 +46,15 @@ def plot_learning_curve(df_train: pd.DataFrame,
     ax.set_ylabel(y)
 
 
-	xticks = df_epoch['step']
-	xticklabels = df_epoch['epoch']
-	if len(xticklabels) > 10:
-		xtick_step = len(df_epoch) // 5
+    xticks = df_epoch['step']
+    xticklabels = df_epoch['epoch']
+    if len(xticklabels) > 10:
+        xtick_step = len(df_epoch) // 5
         xticks = xticks[::xtick_step]
         xticklabels = xticklabels[::xtick_step]
 
-	ax.set_xticks(xticks)
-	ax.set_xticklabels(xticklabels)
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(xticklabels)
 
     ax.legend()
     ax.grid()
